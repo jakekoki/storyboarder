52bedc6a4b4aa77b66f56aa10925d62084b87954
jakekoki <bjkoki@hawaii.edu>
2021-01-03 13:15:24 -1000

    joined text with align json output

--- align_test.py
+++ align_test.py
@@ -10,27 +10,38 @@ def clean_html(raw_html):
 
     return cleantext
 
-def final_output_json():
-    xhtml = open("text_in/text.xhtml", "r")
+def generate_xhtml_dict():
+    xhtml = open("text_in/walden.xhtml", "r")
+    xhtml_dict = {}
     for i, line in enumerate(xhtml.readlines()):
-        print(i, clean_html(line))
+        idx = "f" + str(i)
+        text = clean_html(line)
+        xhtml_dict[idx] = text.strip(" \n")
+    print(xhtml_dict)
+    return xhtml_dict
+
+def process_fragment(fragment, xhtml_dict):
+    for key, val in list(fragment.items()):
+        fragment[key]['text'] = xhtml_dict[key]
+
+    return fragment
 
 def output_json(sync_map, output_dir):
+    xhtml_dict = generate_xhtml_dict()
     for text_path, fragments in sync_map.items():
         text_name = get_name_from_path(text_path)
-        # file_path = os.path.join(output_dir, f'{drop_extension(text_name)}.json')
-        # print(file_path)
-        print(fragments)
-        break
-        # with open(file_path, 'w') as f:
-        #     json.dump(fragments, f, indent=2)
+        file_path = os.path.join(output_dir, f'{drop_extension(text_name)}.json')
+        fragments = process_fragment(fragments, xhtml_dict)
+        with open(file_path, 'w') as f:
+            json.dump(fragments, f, indent=2)
 
 sync_map = align(
     'text_in',
     'audio_in',
     output_dir="audio_out",
-    output_format='json'
+    output_format='json',
+    skip_penalty=0.8,
+    radius=100
 )
 
-# final_output_json()
-# output_json(sync_map, "audio_out")
+output_json(sync_map, "audio_out")
